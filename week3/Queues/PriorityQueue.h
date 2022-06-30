1c919a0919054b3edd16350bfa4bd9d81940bf1b
Veronika <hunyakvera@gmail.com>
2022-06-30 21:44:51 +0100

    week3 - PriorityQueue up to e5

--- week3/Queues/PriorityQueue.h
+++ week3/Queues/PriorityQueue.h
@@ -1,14 +1,68 @@
-//
-// Created by Veronika Eve on 25/06/2022.
-//
+#include <iostream>
 
-#ifndef QUEUES_PRIORITYQUEUE_H
-#define QUEUES_PRIORITYQUEUE_H
-
+using namespace std;
 
 class PriorityQueue {
+public:
+	int count;
+	int size;
+	int *data;
 
+	~PriorityQueue();
+	PriorityQueue(int size);
+
+	void Insert(int Item);
+	bool IsEmpty();
+	bool IsFull();
+	int PeakMin();
+	int Remove();
+	int Size();
 };
 
+PriorityQueue::~PriorityQueue() {}
 
-#endif //QUEUES_PRIORITYQUEUE_H
+PriorityQueue::PriorityQueue(int size) : size(size) {
+	count = 0;
+	data = new int[size];
+}
+
+int PriorityQueue::PeakMin() {
+	return data[count-1];
+}
+
+bool PriorityQueue::IsEmpty() {
+	return count == 0;
+}
+
+bool PriorityQueue::IsFull() {
+	return  count == size;
+}
+
+int PriorityQueue::Size() {
+	return sizeof(*data);
+}
+
+void PriorityQueue::Insert(int Item) {
+	if (IsEmpty()) {
+		data[0] = Item;
+		count++;
+	} else {
+		int j;
+
+		for (j=(count-1); j >= 0; j--) {
+			if (Item > data[j]) {
+				data[(j+1)] = data[j];
+			} else {
+				break;
+			}
+		}
+		data[(j+1)]=Item;
+		count++;
+	}
+}
+
+int PriorityQueue::Remove() {
+	int itemToRemove = data[count-1];
+	count--;
+	return itemToRemove;
+}
--- week3/Queues/main.cpp
+++ week3/Queues/main.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
 #include "Queue.h"
+#include "PriorityQueue.h"
 
 using namespace std;
 
 int main() {
+//
+//	Queue newArray(6);
+//
+//	newArray.Insert(10);
+//	newArray.Insert(20);
+//	newArray.Insert(30);
+//	newArray.Insert(40);
+//	newArray.Insert(50);
+//	newArray.Insert(60);
+//
+//	while (!newArray.IsEmpty()){
+//		cout << "Removing: " << newArray.Remove() << endl;
+//	}
 
-	Queue newArray(6);
+	PriorityQueue newPriorityArray(6);
 
-	newArray.Insert(10);
-	newArray.Insert(20);
-	newArray.Insert(30);
-	newArray.Insert(40);
-	newArray.Insert(50);
-	newArray.Insert(60);
+	newPriorityArray.Insert(43);
+	newPriorityArray.Insert(23);
+	newPriorityArray.Insert(54);
+	newPriorityArray.Insert(11);
+	newPriorityArray.Insert(233);
+	newPriorityArray.Insert(3);
 
-	while (!newArray.IsEmpty()){
-		cout << "Removing: " << newArray.Remove() << endl;
+	for (int i = 0; i < newPriorityArray.size; ++i) {
+		cout << "Item at index " << i << " is: " << newPriorityArray.data[i] << endl;
+	}
+
+	while (!newPriorityArray.IsEmpty()){
+		cout << "Removing: " << newPriorityArray.Remove() << endl;
 	}
 }
