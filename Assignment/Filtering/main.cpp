934520ffbf8c0e418b85ec0efb073f4a1eda062f
Veronika <hunyakvera@gmail.com>
2022-08-06 11:59:26 +0100

    cleanup trial files

--- Assignment/Filtering/main.cpp
+++ Assignment/Filtering/main.cpp
@@ -1,26 +1,9 @@
 #include "common.h"
 
-#include "utils.h"
-
-#include "getAllData.h"
-#include "findData.h"
-#include "filterData.h"
 #include "getAndStoreData.h"
 
 int main() {
 	string filename = "../heroes_information.csv";
-//	filterData(filename, "Female");
-
-//	getAllData(filename);
-//
-
-//	struct FilterType Gender = {1, "Gender"};
-//
-//	map<string, int> filterTypes;
-//
-//	filterTypes.insert(pair<string, int>("Gender",2));
-//	filterTypes.insert(pair<string, int>("EyeColour",  3));
-//
 
 	HeroList *heroList = new HeroList;
 
@@ -31,15 +14,3 @@ int main() {
 
 
 }
-
-
-
-
-//	ifstream fin;
-//	string line;
-//	// Open an existing file
-//	fin.open(filename);
-//	while(!fin.eof()){
-//		fin>>line;
-//		cout<<line << endl;
-//	}
